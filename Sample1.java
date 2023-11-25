8b64420ea830ff1e604023b3d452d76655ea3755
ShashiKanth <57086246+shashikanth-t@users.noreply.github.com>
2023-11-25 07:50:40 +0530

    25112023750-task1

---
+++ Sample1.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+import java.lang.*;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Java Sample Application.....");
+		System.out.println("This is my First Line..");
+
+	}
+
+}
+class Sample2
+{
+	
+}
+}
